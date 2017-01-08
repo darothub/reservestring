6b3ada70c6c4ef339fc5c0b7496c9e9f263a5c8d
DAROT <darotudeen@gmail.com>
2017-01-08 11:32:33 -0800

    commiting better tested findMinMax

--- app/library.js
+++ app/library.js
@@ -1,33 +1,34 @@
 'use strict'
-var file = {}
 
 var findMinMax = function(arr){
-	var Minmax = [];
-	var maxi = arr[0];
-	var mini = arr[arr.length-1]
-	if (!Array.isArray(arr) || arr === 0){
+	if (!Array.isArray(arr)){
 		return "invalid argument type"
   	}
+  	for (var j = 0; j<arr.length; j++){
+  		if (arr[j].constructor === String){
+  			return "invalid argument";
+  		}
+  	}
+  	var Minmax = [];
+	var maxi = arr[0];
+	var mini = arr[0];
   	for(var i=1; i<arr.length; i++){
 		if(arr[i] > maxi){
 			maxi = arr[i];
 		}
-		else if(arr[i]<mini){
+		if(arr[i]<mini){
 			mini = arr[i]
 		}
   	}
+  	
   	Minmax.push(mini);
   	Minmax.push(maxi);
+  	if (mini === maxi){
+  		return [mini];
+  	}
   	return Minmax;
 }
-  	//for(var j=1; j<arr.length; j++){
-	//	if(arr[i] < mini){
-	//		mini = arr[i];
-//		}
-//	}
-  //	Minmax.push(mini;
-  	//return Minmax
-  	
-	
-	
-module.exports = file;
+  	 	
+module.exports = {
+	findMinMax : findMinMax
+};
