188d2b4c3ff98580699ee44f4ce6cd098fceb6ce
DAROT <darotudeen@gmail.com>
2017-01-08 03:48:53 -0800

    committing additional cases

--- app/library.js
+++ app/library.js
@@ -1,43 +1,33 @@
 'use strict'
 var file = {}
-file.findMinMax = function(arr){
+
+var findMinMax = function(arr){
 	var Minmax = [];
-	if (!Array.isArray(arr)){
+	var maxi = arr[0];
+	var mini = arr[arr.length-1]
+	if (!Array.isArray(arr) || arr === 0){
 		return "invalid argument type"
   	}
-  	else{
-  		Minmax.push(getMin(arr));
-  		Minmax.push(getMax(arr));
-  		return Minmax
-  		}
-	}
-	file.getMax =function(n){
-		var maxi = arr[0];
-		if (!Array.isArray(n)){
-			return "invalid argument type"
+  	for(var i=1; i<arr.length; i++){
+		if(arr[i] > maxi){
+			maxi = arr[i];
 		}
-		else{
-			for(var i=1; i<arr.length; i++){
-				if(arr[i] > maxi){
-					maxi = arr[i];
-  				}
-  			}
-  			return maxi
-  		}	
-  
-	}
-	file.getMin = function(x){
-		var mini = arr[0];
-		if (!Array.isArray(x)){
-			return "invalid argument type"
-  		}
-  		else{
-  			for(var i=1; i<arr.length; i++){
-  				if(arr[i] < mini){
-  					mini = arr[i];
-  				}
-  			}
-  			return mini
-  		}
-  
-	}
+		else if(arr[i]<mini){
+			mini = arr[i]
+		}
+  	}
+  	Minmax.push(mini);
+  	Minmax.push(maxi);
+  	return Minmax;
+}
+  	//for(var j=1; j<arr.length; j++){
+	//	if(arr[i] < mini){
+	//		mini = arr[i];
+//		}
+//	}
+  //	Minmax.push(mini;
+  	//return Minmax
+  	
+	
+	
+module.exports = file;
--- tests/tests.js
+++ tests/tests.js
@@ -1,17 +1,23 @@
-(function() {
-  'use strict';
+'use strict';
 
-  describe('Min-Max Numbers in a List: ', function () {
 
-    describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
+var chai = require('jasmine');
+// var assert = chai.assert;
+// var expect = chai.expect;
+// var should = chai.should();
+var myApp = require('../app/library.js');
 
-      it('should return [1,4] for [1, 2, 3 , 4]', function () {
-        expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
-      });
+describe('Min-Max Numbers in a List: ', function () {
 
-      it('should return [4, 6] for [6, 4]', function () {
-        expect(findMinMax([6, 4])).toEqual([4, 6]);
-      });
+  describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
+
+    it('should return [1,4] for [1, 2, 3 , 4]', function () {
+      expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+    });
+
+    it('should return [4, 6] for [6, 4]', function () {
+      expect(findMinMax([6, 4])).toEqual([4, 6]);
+    });
 
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
         expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
@@ -34,6 +40,11 @@
       
     });
 
-  });
+     describe('should return undefined if called with no argument', function(){
+      it('should return undefined', function(){
+        expect(findMinMax().toEqual("invalid"))
+      });
+      
+    });
 
-})();
+});
